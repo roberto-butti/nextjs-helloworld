2b79d63e62b39dba89e5a6e5eba41c85c617f6b7
Roberto Butti <roberto.butti@gmail.com>
2017-06-28 16:06:48 +0200

    components (menu)  and layout

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 // This is the Link API
 import Link from 'next/link'
 
-const Index = () => (
-  <div>
 
-<Link href="/about">
-  <button>Go to About Page</button>
-</Link>
-    <p>Hello Next.js</p>
-  </div>
+import Layout from '../components/mylayout.js'
+
+
+
+const Index = () => (
+<Layout>
+       <p>Hello Next.js</p>
+    </Layout>
 )
 
 export default Index
